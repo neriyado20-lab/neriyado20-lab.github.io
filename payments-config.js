4e3ad0cad127c35a74a9310f3e82f666baf2cad5
neriya doytch <neriyado20@gmail.com>
2026-06-25 23:12:46 +0300

    Publish W028 payment-ready purchase flow

---
+++ payments-config.js
@@ -0,0 +1,17 @@
+window.GAL_EINAI_PAYMENTS = Object.freeze({
+  enabled: false,
+  provider: "PayPlus",
+  supportEmail: "",
+  monthly: {
+    name: "גל עיני מקצועית - חודשי",
+    price: 29,
+    period: "לחודש",
+    paymentUrl: "",
+  },
+  annual: {
+    name: "גל עיני מקצועית - שנתי",
+    price: 249,
+    period: "לשנה",
+    paymentUrl: "",
+  },
+});
